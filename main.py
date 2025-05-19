34c12775851d32d2907bb75f0bbc28e999a7202e
Francois Moumbe <121656576+s4fmoumbe@users.noreply.github.com>
2025-05-19 11:45:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def hello():
 def bye():
     print("bye")
 
-# Call the  functions
-    hello()   # This will print "hi"
-    bye()     # This will print "bye"
 
+# Call the functions
+hello()
+bye()
